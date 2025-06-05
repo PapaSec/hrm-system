00f250da1589b34985c289f329e6054f3845f55e
PapaSec <fransronald@gmail.com>
2025-06-05 18:31:40 +0200

    Daily commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,5 @@
+<div class="relative mb-6 w-full">
+    <flux:heading size="xl" level="1">{{ $heading }}</flux:heading>
+    <flux:subheading size="lg" class="mb-6">{{ $sub_heading??'' }}</flux:subheading>
+    <flux:separator variant="subtle" />
+</div>
---
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -0,0 +1,13 @@
+<?php
+
+use Livewire\Volt\Component;
+
+new class extends Component {
+    //
+}; ?>
+
+<div>
+    <x-header>
+        <x-slot:heading>Dashboard</x-slot:heading>
+    </x-header>
+</div>
